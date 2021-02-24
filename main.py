07a822c3efece90e5a03b4574bb5df07a7204627
Christian <christian.vinther.thomsen@gmail.com>
2021-02-24 21:05:01 +0100

    Add data formatter class which groups the format functions in a format class. Furthoermore the data formatter class now contains the compile function which was previously called match_datetimes.

--- main.py
+++ main.py
@@ -26,137 +26,155 @@ def create_table(url, index):
     df = pd.DataFrame(p.tables[0])
     return df
 
-def format_price_contents(year):
-    url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yieldYear&year=" + year
-    df = create_table(url, 0)
-    col_names = df[0:1].values.tolist()[0]
-    df = df.drop(0, axis=0)
-    col_names.pop(0)
-    col_names.insert(0, "date")
-    df.columns = col_names
-    date_list = df["date"].to_list()
-    new_date_list = []
-    for e in date_list:
-        dates = e.split("/")
-        date = datetime(int("20"+dates[2]), int(dates[0]), int(dates[1]))
-        new_date_list.append(date)
-    df["date"] = new_date_list
-    return df
 
-def format_networth(file):
-    needed_col = ["date", "net_mean_worth", "net_median_worth"]
-    for e in file.columns.to_list():
-        if e not in needed_col:
-            file = file.drop(e, axis=1)
-    new_date_list = []
-    for e in file["date"].to_list():
-        date = datetime(int(e), 1, 1)
-        new_date_list.append(date)
-    file["date"] = new_date_list
-    return file
+class data_formatter():
+    def __init__(self):
+        self.format = self._format()
+    class _format():
+        def __init__(self):
+            self.selected_features = []
 
-def format_median_networth():
-    df = pd.read_csv("data/median_networth.csv")
-    df = format_networth(df)
-    return df
+        def _single_yield(self, year):
+            url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yieldYear&year=" + year
+            df = create_table(url, 0)
+            col_names = df[0:1].values.tolist()[0]
+            df = df.drop(0, axis=0)
+            col_names.pop(0)
+            col_names.insert(0, "date")
+            df.columns = col_names
+            date_list = df["date"].to_list()
+            new_date_list = []
+            for e in date_list:
+                dates = e.split("/")
+                date = datetime(int("20" + dates[2]), int(dates[0]), int(dates[1]))
+                new_date_list.append(date)
+            df["date"] = new_date_list
+            return df
 
-def format_mean_networth():
-    df = pd.read_csv("data/mean_networth.csv")
-    df = format_networth(df)
-    return df
+        def bond_yield(self, years):
+            full_df = pd.DataFrame()
+            for year in years:
+                print("Extracting yeary:", year)
+                ret_df = self._single_yield(year)
+                full_df = full_df.append(ret_df)
+            full_df = full_df.reset_index(drop=True)
+            print(full_df)
+            self.bond_yield = full_df
 
-def format_discount_rate():
-    discount_df = pd.read_csv("data/dicount_rate.csv")
-    discount_df.columns = ["date", "discount_date"]
-    discount_df.to_csv("data/discount_rate.csv", index=False)
-    date_list = discount_df["date"].to_list()
-    new_date_list = []
-    for e in date_list:
-        dates = e.split("-")
-        date = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
-        new_date_list.append(date)
-    discount_df["date"] = new_date_list
-    return discount_df
+        def _format_networth(self, file):
+            needed_col = ["date", "net_mean_worth", "net_median_worth"]
+            for e in file.columns.to_list():
+                if e not in needed_col:
+                    file = file.drop(e, axis=1)
+            new_date_list = []
+            for e in file["date"].to_list():
+                date = datetime(int(e), 1, 1)
+                new_date_list.append(date)
+            file["date"] = new_date_list
+            file = file.reset_index(drop=True)
+            return file
 
-def format_median_income():
-    income_df = pd.read_csv("data/income.csv")
-    income_df.columns = ["date", "median_income"]
-    income_df.to_csv("data/income.csv", index=False)
-    date_list = income_df["date"].to_list()
-    new_date_list = []
-    for e in date_list:
-        dates = e.split("-")
-        date = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
-        new_date_list.append(date)
-    income_df["date"] = new_date_list
-    return income_df
+        def median_networth(self):
+            df = pd.read_csv("data/median_networth.csv")
+            df = self._format_networth(df)
+            self.selected_features.append(df)
 
-def format_avg_income():
-    url = "https://dqydj.com/household-income-by-year/"
-    df = create_table(url, 0)
-    df = df.drop(0, axis=0)
-    df.columns = ["date", "avg. income", "inflation adj.(2020)"]
-    dates = df["date"].to_list()
-    new_date_list = []
-    for e in dates:
-        date = datetime(int(e), 1, 1)
-        new_date_list.append(date)
-    df["date"] = new_date_list
-    return df
+        def mean_networth(self):
+            df = pd.read_csv("data/mean_networth.csv")
+            df = self.format_networth(df)
+            self.selected_features.append(df)
 
-def format_reservereq_contents():
-    p = parse_html("https://www.federalreserve.gov/monetarypolicy/reservereq.htm")
-    reserve_req = pd.DataFrame(p.tables[1])
-    reserve_req = reserve_req.drop(0, axis=0)
-    reserve_req.columns = ["date", "low_reserve_trance", "exemption_amount"]
-    date_list = reserve_req["date"].to_list()
-    new_date_list = []
-    for e in date_list:
-        dates = e.split(" ")
-        day = dates[1].split(",")[0]
-        date = datetime(int(dates[2]), int(month_dict[dates[0]]), int(day))
-        new_date_list.append(date)
-    reserve_req["date"] = new_date_list
-    reserve_req = reserve_req.reset_index(drop=True)
-    return reserve_req
+        def discount_rate(self):
+            df = pd.read_csv("data/dicount_rate.csv")
+            df.columns = ["date", "discount_date"]
+            df.to_csv("data/discount_rate.csv", index=False)
+            date_list = df["date"].to_list()
+            new_date_list = []
+            for e in date_list:
+                dates = e.split("-")
+                date = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
+                new_date_list.append(date)
+            df["date"] = new_date_list
+            df = df.reset_index(drop=True)
+            self.selected_features.append(df)
 
-def get_multiple_years(years):
-    full_df = pd.DataFrame()
-    for year in years:
-        print("Extracting yeary:", year)
-        ret_df = format_price_contents(year)
-        full_df = full_df.append(ret_df)
-    full_df = full_df.reset_index(drop=True)
-    return full_df
+        def median_income(self):
+            df = pd.read_csv("data/income.csv")
+            df.columns = ["date", "median_income"]
+            df.to_csv("data/income.csv", index=False)
+            date_list = df["date"].to_list()
+            new_date_list = []
+            for e in date_list:
+                dates = e.split("-")
+                date = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
+                new_date_list.append(date)
+            df["date"] = new_date_list
+            df = df.reset_index(drop=True)
+            self.selected_features.append(df)
 
-def match_datetimes(org_frame, comp_frame):
-    list_dict = {}
-    for i in comp_frame.columns.to_list():
-        list_dict[i] = []
-    current_index = 0
-    for e in range(len(comp_frame)):
-        if comp_frame.at[e,"date"] >= org_frame.at[0,"date"]:
-            current_index = e-1
-            break
-    for e in range(len(org_frame)):
-        org_frame_date = org_frame.at[e, "date"]
-        comp_cur_date = comp_frame.at[current_index, "date"]
-        if current_index+1 != len(comp_frame):
-            comp_next_date = comp_frame.at[current_index+1, "date"]
-        if org_frame_date >= comp_cur_date and org_frame_date < comp_next_date:
-            for i in list_dict:
-                list_dict[i].append(comp_frame.at[current_index, i])
-        elif current_index+1 < len(comp_frame):
-            for i in list_dict:
-                list_dict[i].append(comp_frame.at[current_index+1, i])
-            current_index += 1
-        else:
-            for i in list_dict:
-                list_dict[i].append(comp_frame.at[len(comp_frame)-1, i])
-    for e in list_dict:
-        if e != "date":
-            org_frame[e] = list_dict[e]
-    return org_frame
+        def avg_income(self):
+            url = "https://dqydj.com/household-income-by-year/"
+            df = create_table(url, 0)
+            df = df.drop(0, axis=0)
+            df.columns = ["date", "avg. income", "inflation adj.(2020)"]
+            dates = df["date"].to_list()
+            new_date_list = []
+            for e in dates:
+                date = datetime(int(e), 1, 1)
+                new_date_list.append(date)
+            df["date"] = new_date_list
+            df = df.reset_index(drop=True)
+            self.selected_features.append(df)
+
+        def reserve_requirements(self):
+            p = parse_html("https://www.federalreserve.gov/monetarypolicy/reservereq.htm")
+            df = pd.DataFrame(p.tables[1])
+            df = df.drop(0, axis=0)
+            df.columns = ["date", "low_reserve_trance", "exemption_amount"]
+            date_list = df["date"].to_list()
+            new_date_list = []
+            for e in date_list:
+                dates = e.split(" ")
+                day = dates[1].split(",")[0]
+                date = datetime(int(dates[2]), int(month_dict[dates[0]]), int(day))
+                new_date_list.append(date)
+            df["date"] = new_date_list
+            df = df.reset_index(drop=True)
+            self.selected_features.append(df)
+
+    def compile(self):
+        org_frame = self.format.bond_yield
+        for comp_frame in self.format.selected_features:
+            print(comp_frame.to_string())
+            list_dict = {}
+            for i in comp_frame.columns.to_list():
+                list_dict[i] = []
+            current_index = 0
+
+            for e in range(len(comp_frame)):
+                if comp_frame.at[e, "date"] >= org_frame.at[0, "date"]:
+                    current_index = e - 1
+                    break
+            for e in range(len(org_frame)):
+                org_frame_date = org_frame.at[e, "date"]
+                comp_cur_date = comp_frame.at[current_index, "date"]
+                if current_index + 1 != len(comp_frame):
+                    comp_next_date = comp_frame.at[current_index + 1, "date"]
+                if org_frame_date >= comp_cur_date and org_frame_date < comp_next_date:
+                    for i in list_dict:
+                        list_dict[i].append(comp_frame.at[current_index, i])
+                elif current_index + 1 < len(comp_frame):
+                    for i in list_dict:
+                        list_dict[i].append(comp_frame.at[current_index + 1, i])
+                    current_index += 1
+                else:
+                    for i in list_dict:
+                        list_dict[i].append(comp_frame.at[len(comp_frame) - 1, i])
+            for e in list_dict:
+                if e != "date":
+                    org_frame[e] = list_dict[e]
+        return org_frame
+
 
 def evaluate_model(model, test_features, test_labels):
     pred = model.predict(test_features)
@@ -232,5 +250,13 @@ print(full_frame.to_string())
 income_df = format_income()
 print(income_df)"""
 
-df = format_median_networth()
+year = ["2018", "2019", "2020"]
+
+data = data_formatter()
+data.format.bond_yield(year)
+data.format.avg_income()
+data.format.median_networth()
+df = data.compile()
 print(df.to_string())
+
+
