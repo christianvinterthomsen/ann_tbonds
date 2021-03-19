b5cdf3a5c3ee2b1594e6ea463ae7e17e2a2feb00
Christian <christian.vinther.thomsen@gmail.com>
2021-03-19 10:43:03 +0100

    Added code formatting, analysis functions and trained models

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+from keras.callbacks import EarlyStopping
+from keras.models import load_model
+import json
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 month_dict = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8,
               "September": 9, "October": 10, "November": 11, "December": 12}
@@ -26,6 +31,40 @@ def create_table(url, index):
     p = parse_html(url)
     df = pd.DataFrame(p.tables[0])
     return df
+
+def clean_frame(frame, drop_value):
+    for e in frame.columns.to_list():
+        if e != "date":
+            for i in drop_value:
+                list = frame[e].to_list()
+                new_list = []
+                for j in list:
+                    new_list.append((str(j).replace(i, "")))
+                frame[e] = new_list
+    return frame
+
+def compact_list(list):
+    ret_list = []
+    for e in list:
+        for i in e:
+            ret_list.append(i)
+    return ret_list
+
+def round_list(list, dec_points):
+    ret_list = []
+    for e in list:
+        ret_list.append(round(e, dec_points))
+    return ret_list
+
+
+def dump_json(filename, data):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+def open_json(filename):
+    with open(filename) as infile:
+        data = json.load(infile)
+    return data
 
 
 class data_formatter():
@@ -69,7 +108,16 @@ class data_formatter():
                 full_df = full_df.append(ret_df)
             full_df = full_df.reset_index(drop=True)
             print(full_df)
-            self.bond_yield = full_df
+            self.bond_yield_df = full_df
+
+        def pre_yield(self):
+            for col in self.bond_yield_df.columns.to_list():
+                if col != "date":
+                    pre_col = self.bond_yield_df[col]
+                    pre_col.index += 1
+                    print(pre_col)
+                    self.bond_yield_df["pre_" + col] = pre_col
+            return self.bond_yield_df
 
         def _format_networth(self, file):
             needed_col = ["date", "net_mean_worth", "net_median_worth"]
@@ -91,7 +139,7 @@ class data_formatter():
 
         def mean_networth(self):
             df = pd.read_csv("data/mean_networth.csv")
-            df = self.format_networth(df)
+            df = self._format_networth(df)
             self.selected_features.append(df)
 
         def discount_rate(self):
@@ -286,14 +334,14 @@ class data_formatter():
             self.selected_features.append(df)
 
     def compile(self):
-        org_frame = self.format.bond_yield
+        org_frame = self.format.bond_yield_df
         for comp_frame in self.format.selected_features:
             list_dict = {}
             for i in comp_frame.columns.to_list():
                 list_dict[i] = []
             current_index = 0
 
-            for e in range(len(comp_frame)):
+            for e in range(1,len(comp_frame)):
                 if comp_frame.at[e, "date"] >= org_frame.at[0, "date"]:
                     current_index = e - 1
                     break
@@ -317,6 +365,158 @@ class data_formatter():
                     org_frame[e] = list_dict[e]
         return org_frame
 
+class analyzer():
+    def __init__(self, model, frame, search_col):
+        self.model = model
+        self.frame = frame
+        self.search_col = search_col
+        self.true = frame[search_col]
+
+    def set_data_groups(self, split):
+        self.training_features, self.training_labels, \
+        self.test_features, self.test_labels, self.training_dates, self.test_dates = prepare_training_data(self.frame, split, self.search_col)
+        self.get_prediction()
+        self.compact_predictions()
+
+    def get_prediction(self):
+        self.training_pred = round_list(compact_list(self.model.predict(self.training_features)),2)
+        self.test_pred = round_list(compact_list(self.model.predict(self.test_features)),2)
+
+    def compact_predictions(self):
+        full_pred_list = []
+        full_label_list = []
+
+        full_pred_list.append(self.training_pred)
+        full_pred_list.append(self.test_pred)
+
+        full_label_list.append(self.training_labels)
+        full_label_list.append(self.test_labels)
+
+        self.full_true = compact_list(full_label_list)
+        self.full_pred = compact_list(full_pred_list)
+
+    def plot_prediction(self, pred, true, dates):
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=75))
+        plt.plot(dates, pred, label="Forudsigelse")
+        plt.plot(dates, true, label="Reelle værdier")
+        plt.xlabel("Dato")
+        plt.ylabel("Direkte rente")
+        plt.gcf().autofmt_xdate()
+        plt.title(f"Forudsigelse og reelle værdier \n for US. Treasuries Bonds med løbetid på 10 år \n for test data i perioden {dates[0]} til {dates[len(dates)-1]}")
+        plt.legend()
+        plt.show()
+
+    def plot_history(self, history):
+        acc = history['mae']
+        val_acc = history['val_mae']
+        loss = history['loss']
+        val_loss = history['val_loss']
+
+        epochs = range(1, len(acc) + 1)
+        """plt.plot(epochs, acc, label='Training acc')
+        plt.plot(epochs, val_acc, label='Validation acc')
+        plt.title('Training and validation accuracy')
+        plt.legend()
+
+        plt.figure()"""
+
+        plt.plot(epochs, loss, label='Trænings tab(MAE)')
+        plt.plot(epochs, val_loss, label='Test tab(MAE)')
+        plt.title('Oversigt over tab under træning, \n for den direkte rente på US Treasuries med løbetid på 10 år')
+        plt.xlabel("Epoch")
+        plt.ylabel("Tab(MAE)")
+        plt.legend()
+
+        plt.show()
+
+    def _calc_residiuals(self, pred, true):
+        dif_list = []
+        for e in range(len(true)):
+            dif_list.append(abs(true[e] - pred[e]))
+        return dif_list
+
+    def calc_rsquared(self,pred, true):
+        res = self._calc_residiuals(true, pred)
+        res_list = []
+        for e in res:
+            res_list.append(e ** 2)
+
+        avg = sum(true) / len(true)
+        avg_dif = []
+        for e in true:
+            avg_dif.append((e - avg) ** 2)
+
+        lower = sum(avg_dif)
+        upper = sum(res_list)
+
+        rsquared = 1 - (upper/lower)
+        print("Modellen for obligationer med løbetid på 20 år")
+        print("R2:", rsquared)
+
+    def calc_rsquared_adjusted(self,pred, true):
+        K = len(self.frame.columns.to_list())-1
+        n = len(self.frame)
+        res = self._calc_residiuals(true, pred)
+        res_list = []
+        for e in res:
+            res_list.append(e ** 2)
+
+        avg = sum(true) / len(true)
+        avg_dif = []
+        for e in true:
+            avg_dif.append((e - avg) ** 2)
+
+        lower = sum(avg_dif)
+        upper = sum(res_list)
+
+        rsquared_adjusted = 1 - ((upper/(n-K))/(lower/(n-1)))
+        print("R2 justeret:", rsquared_adjusted)
+
+    def calc_direc_succes_rate(self, pred, true):
+        base = true[0:len(true) - 1]
+        true = true[1:len(true)]
+        pred = pred[1:len(pred)]
+        down_correct = []
+        up_correct = []
+        neutral_correct = []
+        neutral_wrong = []
+        down_wrong = []
+        up_wrong = []
+        for e in range(len(base)):
+            if pred[e] > base[e]:
+                if true[e] > base[e]:
+                    up_correct.append(e)
+                else:
+                    up_wrong.append(e)
+            elif pred[e] < base[e]:
+                if true[e] < base[e]:
+                    down_correct.append(e)
+                else:
+                    down_wrong.append(e)
+            elif pred[e] == base[e]:
+                if true[e] == base[e]:
+                    neutral_correct.append(e)
+                else:
+                    neutral_wrong.append(e)
+        print("Fordeling af forudset retning \n for modellen for obligationer \n med løbetid på 10 år")
+        print("Antal")
+        num_df = pd.DataFrame({"Retning": ["Op", "Samme", "Ned", "Sum"],
+                           "Korrekt": [len(up_correct), len(neutral_correct), len(down_correct), len(up_correct)+len(neutral_correct)+len(down_correct)],
+                           "Forkert": [len(up_wrong), len(neutral_wrong), len(down_wrong), len(up_wrong) + len(neutral_wrong) + len(down_wrong)]},
+                            )
+        print(num_df.to_string())
+        print("Procent")
+        #+1 -1 tilføjet for at forhindre dividering med 0
+        dec_point = 2
+        pct_df = pd.DataFrame({"Retning": ["Op", "Samme", "Ned", "Sum"],
+                           "Korrekt": [round(len(up_correct)/(len(up_correct)+len(up_wrong))*100, dec_point), round(((len(neutral_correct)+1)/(len(neutral_correct)+len(neutral_correct)+1)-1)*100, dec_point), round(len(down_correct)/(len(down_wrong)+len(down_correct))*100, dec_point),
+                                       round((len(up_correct) + len(neutral_correct) + len(down_correct))/(len(up_correct) + len(neutral_correct) + len(down_correct)+ len(up_wrong) + len(neutral_wrong) + len(down_wrong))*100,dec_point)],
+                           "Forkert": [round((len(up_wrong)/(len(up_correct)+len(up_wrong))*100), dec_point), round((((len(neutral_wrong)+1)/(len(neutral_correct)+len(neutral_correct)+1)-1)*100),dec_point), round(len(down_wrong)/(len(down_wrong)+len(down_correct))*100,dec_point),
+                                       round(((len(up_wrong) + len(neutral_wrong) + len(down_wrong))/((len(up_correct) + len(neutral_correct) + len(down_correct)+ len(up_wrong) + len(neutral_wrong) + len(down_wrong)))*100), dec_point)]},
+                          )
+        print(pct_df.to_string())
+        return df
+
 
 def evaluate_model(model, test_features, test_labels):
     pred = model.predict(test_features)
@@ -328,9 +528,10 @@ def evaluate_model(model, test_features, test_labels):
     df = pd.DataFrame({"change": change_list, "pred_dif": dif_list})
     print("Average change:",df["change"].mean())
     print("Average residual:", df["pred_dif"].mean())
+    return df["pred_dif"].mean()
 
 def prepare_training_data(frame, test_split, test_col):
-    frame = frame.drop("date", axis=1)
+
     frame = frame.reset_index(drop=True)
     labels = []
     for e in range(len(frame)-1):
@@ -338,67 +539,87 @@ def prepare_training_data(frame, test_split, test_col):
     training_features = frame[0: int(len(frame)*(1-test_split))]
     test_features = frame[int(len(frame)*(1-test_split)): len(frame)-1]
     training_labels = labels[0: int(len(frame)*(1-test_split))]
+    training_dates = training_features["date"].to_list()
+    test_dates = test_features["date"].to_list()
+    training_features = training_features.drop("date", axis=1)
+    test_features = test_features.drop("date", axis=1)
     test_labels = labels[int(len(frame)*(1-test_split)): len(frame)-1]
     scaler = MinMaxScaler().fit(training_features)
     return scaler.transform(training_features), np.array(training_labels), \
-           scaler.transform(test_features), np.array(test_labels)
+           scaler.transform(test_features), np.array(test_labels), training_dates, test_dates
 
 def train_model(frame, test_split, test_col):
     training_features, training_labels, test_features, test_labels = prepare_training_data(frame, test_split, test_col)
-    print(len(training_labels))
     model = Sequential([
-        Dense(15, activation='relu', input_shape=[0,len(training_features[0])]),
-        Dense(30, activation='relu'),
-        Dense(100, activation='relu'),
-        Dense(10, activation='relu'),
-        Dense(1, activation='linear')
+        Dense(20, activation='relu'),
+        Dense(50, activation='relu'),
+        Dense(1, activation='relu')
     ])
-    model.compile(optimizer='adam', loss='mae', metrics=['acc'])
-    model.fit(training_features, training_labels, batch_size=1, epochs=50)
-    evaluate_model(model, test_features, test_labels)
-    return model
+    es = EarlyStopping(monitor='val_mae', verbose=1, patience=5)
+    model.compile(optimizer='sgd', loss='mae', metrics=['mae'], )
+    hist = model.fit(training_features, training_labels, batch_size=16, epochs=25,
+                     validation_data=(test_features, test_labels))
+    model.save("model")
+    dump_json("hist", hist.history)
+    res = evaluate_model(model, test_features, test_labels)
+    return model, hist.history
 
+
+def compile_new_frame(years):
+    data = data_formatter()
+    data.format.bond_yield(years)
+    data.format.median_networth()
+    data.format.mean_networth()
+    data.format.discount_rate()
+    data.format.avg_income()
+    data.format.median_income()
+    data.format.reserve_requirements()
+    data.format.CPI()
+    data.format.inflation()
+    data.format.outstanding_con_credit()
+    data.format.financial_literacy()
+    data.format.age_distribution()
+    data.format.sp500()
+    data.format.VSTMX()
+    data.format.goverment_rev()
+    data.format.goverment_expend()
+    data.format.fed_bond_holdings()
+    data.format.GDP_growth()
+    return data.compile()
+
+def save(frame):
+    frame.to_csv("compiled_frame", index=False)
+
+def create_new_frame(year):
+    df = compile_new_frame(year)
+    df = clean_frame(df, ["$", ","])
+    save(df)
+    return df
 
 
 """years = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008",
          "2009", "2010", "2011","2012","2013","2014","2015","2016","2017","2018",
          "2019","2020"]
-#years = ["2020"]
-price_df = get_multiple_years(years)
-discount_df = format_discount_rate()
-reservereq_df = format_reservereq_contents()
-income_df = format_income()
-
-full_frame = match_datetimes(price_df, discount_df)
-full_frame = match_datetimes(full_frame, reservereq_df)
-full_frame = match_datetimes(full_frame, income_df)
-
-for c in full_frame.columns:
-    full_frame[c] = pd.to_numeric(price_df[c], errors='coerce')
-full_frame = full_frame.fillna(0)
-
-model = train_model(full_frame, 0.1, "30 yr")"""
-"""
-discount_df = format_discount_rate()
-print(discount_df.to_string())
-reservereq_df = format_reservereq_contents()
-print(reservereq_df)
-print(reservereq_df.columns.to_list())
-
-full_frame = match_datetimes(price_df, discount_df)
-full_frame = match_datetimes(full_frame, reservereq_df)
-print(full_frame.to_string())
-
-income_df = format_income()
-print(income_df)"""
-
-year = ["2018", "2019", "2020"]
-year = ["2018"]
-data = data_formatter()
-data.format.bond_yield(year)
-data.format.GDP_growth()
-df = data.compile()
-print(df.to_string())
+df = create_new_frame(years)"""
 
 
 
+
+
+"""df = pd.read_csv("compiled_frame")
+print(df.head().to_string())
+df = df.fillna(0)
+train_model(df, 0.1, "10 yr")"""
+
+df = pd.read_csv("compiled_frame")
+df = df.fillna(0)
+model = load_model("model_10YR_best")
+
+analyzer_obj = analyzer(model, df, "10 yr")
+analyzer_obj.set_data_groups(0.1)
+hist = open_json("hist_10YR_best")
+analyzer_obj.plot_history(hist)
+analyzer_obj.plot_prediction(analyzer_obj.test_pred, analyzer_obj.test_labels, analyzer_obj.test_dates)
+analyzer_obj.calc_direc_succes_rate(analyzer_obj.test_pred, analyzer_obj.test_labels)
+analyzer_obj.calc_rsquared(analyzer_obj.test_pred, analyzer_obj.test_labels)
+analyzer_obj.calc_rsquared_adjusted(analyzer_obj.test_pred, analyzer_obj.test_labels)
